ff850df71cb68beb44a8e802f8c423fedb652d36
Johannes Plunien <plu@pqpq.de>
2016-08-17 08:49:52 +0400

    Clean up trailing whitespace

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -4,12 +4,12 @@ class Fbsimctl < Formula
   url "https://github.com/facebook/FBSimulatorControl/tarball/v0.1.1"
   sha256 "f971c28898457cc21d48542ddf6d269ee931190d702fe07083f4d2cddcb11fc4"
   head "https://github.com/facebook/FBSimulatorControl.git"
-  
+
   depends_on "carthage"
   depends_on :xcode => ["7", :build]
 
   def install
-    system "./build.sh", "fbsimctl", "build", "#{libexec}" 
+    system "./build.sh", "fbsimctl", "build", "#{libexec}"
     bin.install_symlink "#{libexec}/fbsimctl"
   end
 
