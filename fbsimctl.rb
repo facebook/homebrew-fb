fc929c74b7dce6e8f2dd772ea713a611ce10bf44
lawrencelomax <lawrencelomax@fb.com>
2016-10-28 16:39:23 +0100

    Add v0.2.2 tag for fbsimctl

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -1,8 +1,8 @@
 class Fbsimctl < Formula
   desc "A Powerful Command Line for Managing iOS Simulators"
   homepage "https://github.com/facebook/FBSimulatorControl/fbsimctl/README.md"
-  url "https://github.com/facebook/FBSimulatorControl/tarball/v0.2.0"
-  sha256 "872b8b98427527cad16c8f235ae987fc2fad3be00dc344632de489a5cce0f620"
+  url "https://github.com/facebook/FBSimulatorControl/tarball/v0.2.2"
+  sha256 "119718ee8a0611d087f46dda1603c6622b075270ef47889c2b16d781391e7c2b"
   head "https://github.com/facebook/FBSimulatorControl.git"
 
   depends_on "carthage"
