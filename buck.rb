032cff72ba17c2a2b9a2f4a9177b0dc5e3a51e95
Jakub Zika <jakubzika@fb.com>
2016-11-11 13:14:28 +0000

    Update `buck.rb` to v2016.11.11.01

--- buck.rb
+++ buck.rb
@@ -1,18 +1,18 @@
 require "open3"
 
 class Buck < Formula
-  @@buck_version = "2016.09.26.01"
+  @@buck_version = "2016.11.11.01"
   desc "The Buck build system"
   homepage "https://buckbuild.com/"
   head "https://github.com/facebook/buck.git"
   version @@buck_version
-  url "https://api.github.com/repos/facebook/buck/tarball/v2016.09.26.01"
-  sha256 "a8e890c217ead5b9d26a6c9b264b62d78a7ee8ff07f6259203796115b1ee07f6"
+  url "https://api.github.com/repos/facebook/buck/tarball/v2016.11.11.01"
+  sha256 "5b27696a8eb532fa7fecbff458bfd406de861dfc8ef5f551ee43c4c9b0c6efd8"
 
   bottle do
     root_url "https://github.com/facebook/buck/releases/download/v#{@@buck_version}"
     cellar :any_skip_relocation
-    sha256 "2b10373e16e0d72745ccea8b8e7cceed19c477308b94750447ed5c6f06426540" => :yosemite_or_later
+    sha256 "d8ea7fd04d45e9d2ec63feaaee26fd0093ff6ccbc995672f179d86ae091d5bb9" => :yosemite_or_later
   end
 
   depends_on :java => "1.8+"
