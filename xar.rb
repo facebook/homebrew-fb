a4d04aeca17d4e545950f48fa31230d519c39d6c
Sean Karlage <skarlage@fb.com>
2018-07-11 13:04:51 -0700

    XAR: facebook -> facebookincubator
    This will be published to facebookincubator first.

--- xar.rb
+++ xar.rb
@@ -1,7 +1,7 @@
 class Xar < Formula
   desc "The eXecutable Archive Format"
-  homepage "https://github.com/facebook/xar"
-  url "https://github.com/facebook/xar/archive/master.zip"
+  homepage "https://github.com/facebookincubator/xar"
+  url "https://github.com/facebookincubator/xar/archive/master.zip"
   version "0.0.1"
   sha256 "c94bb886ad9bd91ff3d1a65c58b7954b44b64d84ba2abc133426db93a112b9ef"
 
