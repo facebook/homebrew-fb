facebook/homebrew-fb

commit 61caae4561a488c74acf9f8051bc413474237654

[buck] Update buck formula for Homebrew changes (#30)
- Handle a change in homebrew that made it so we don't extract files
properly
- Handle the fact that we changed the name of build to ant-out

buck.rb
@@ -20,11 +20,15 @@ class Buck < Formula
   depends_on "ant"
 
   def install
-    # First, bootstrap the build by building Buck with Apache Ant.
+    # https://github.com/Homebrew/brew/pull/4552 stopped extracting stuff for us
+    if File.exists?("v#{@@buck_version}") then
+      ohai "Homebrew didn't extract the source tarball. Extracting..."
+      system("tar", "--strip-components", "1", "-xf", "v#{@@buck_version}")
+    end
     ohai "Bootstrapping buck with ant"
     system "ant"
     # Mark the build as successful.
-    File.open("build/successful-build", "w") {}
+    File.open("ant-out/successful-build", "w") {}
     # Now, build the Buck PEX archive with the Buck bootstrap.
     ohai "Building buck with buck"
     mkdir_p "#{bin}"

commit 4e64a3a2765c22c552cd998bdfb1853fbb169388

Remove workaround (with underlying issues fixed)
The workaround introduced in 61caae4 can be rolled back now that the issue has been fixed on Homebrew’s side [1].

An additional safety net is that `url` no longer points to the legacy GitHub URL. This means the downloaded file will have the correct file extension to begin with.

[1]: https://github.com/Homebrew/brew/pull/4552

buck.rb
@@ -17,11 +17,7 @@ class Buck < Formula
   depends_on :java => "1.8+"
 
   def install
-    # https://github.com/Homebrew/brew/pull/4552 stopped extracting stuff for us
-    if File.exist?("v#{version}")
-      ohai "Homebrew didn't extract the source tarball. Extracting..."
-      system("tar", "--strip-components", "1", "-xf", "v#{version}")
-    end
+    # First, bootstrap the build by building Buck with Apache Ant.
     ohai "Bootstrapping buck with ant"
     system "ant"
     # Mark the build as successful.