f16efebdac8ebd7b555135929bf3ca8bda7b149c
Zeyad Salloum <zsalloum@fb.com>
2019-04-28 14:26:46 -0700

    1.0.3

--- idb-companion.rb
+++ idb-companion.rb
@@ -1,14 +1,14 @@
 class IdbCompanion < Formula
   desc "A Powerful Command Line for automating iOS Simulators"
   homepage "https://github.com/facebook/idb/README.md"
-  url "https://github.com/facebook/idb/archive/v1.0.2.tar.gz"
-  sha256 "0c509365ba7d5ff49ccc5492d36018afb5ddf9fb5a71411174c455d447e37bee"
+  url "https://github.com/facebook/idb/archive/1.0.3.tar.gz"
+  sha256 "80ff7e8ebe093e85005d4de0978fc5f5c037a7556951b0702eb7b8b14739b19a"
   head "https://github.com/facebook/idb.git"
 
   bottle do
-    root_url "https://github.com/facebook/idb/releases/download/v1.0.2"
+    root_url "https://github.com/facebook/idb/releases/download/1.0.3"
     cellar :any
-    sha256 "e4141795dc0b030c23cf03990f387e28633f4e8abd0e82e861ceca54b5597198" => :mojave
+    sha256 "0bf22cd7d1512533a53a69380653825f3f5c0af3215a62e8249e2872d81eb05d" => :mojave
   end
 
   depends_on :xcode => ["8.2", :build]
