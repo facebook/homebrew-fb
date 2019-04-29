1ed3d69a21144c3918566fb15abe0b1747016c53
Callum Ryan <c.ryan747@gmail.com>
2019-04-29 15:00:29 -0700

    Update idb-companion.rb

--- idb-companion.rb
+++ idb-companion.rb
@@ -1,12 +1,12 @@
 class IdbCompanion < Formula
   desc "A Powerful Command Line for automating iOS Simulators"
   homepage "https://github.com/facebook/idb/README.md"
-  url "https://github.com/facebook/idb/archive/v1.0.4.tar.gz"
-  sha256 "a05440e07b42c57f525c971cba569a294d640010d3cc8f7934375600d444175e"
+  url "https://github.com/facebook/idb/archive/v1.0.5.tar.gz"
+  sha256 "125cce6b420ad8cfa351024f0697c97b6c4ba427f4ee83999bf924ff1c82fd25"
   head "https://github.com/facebook/idb.git"
 
   bottle do
-    root_url "https://github.com/facebook/idb/releases/download/v1.0.4"
+    root_url "https://github.com/facebook/idb/releases/download/v1.0.5"
     cellar :any
     sha256 "941ef3e83afe3a7840a8d8b59ee97532fbdd907d8c78089f7ccde07e763315db" => :mojave
   end
