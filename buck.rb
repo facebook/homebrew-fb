fd2795b21e95b046338fa7a61142284ea6013f4a
Philip Jameson <pjameson@fb.com>
2019-05-06 13:39:49 -0700

    Bump buck to version 2019.05.06.01
    This commit was generated by release automation

--- buck.rb
+++ buck.rb
@@ -1,16 +1,16 @@
 class Buck < Formula
-  BUCK_VERSION = "2019.01.10.01".freeze
-  BUCK_RELEASE_TIMESTAMP = "1547139889".freeze
+  BUCK_VERSION = "2019.05.06.01".freeze
+  BUCK_RELEASE_TIMESTAMP = "1557197409".freeze
   desc "The Buck build system"
   homepage "https://buckbuild.com/"
   url "https://github.com/facebook/buck/archive/v#{BUCK_VERSION}.tar.gz"
-  sha256 "5dbfeb1146c7441ec0061f4c0a905014999328c653acd94558eb2af4a4bf2fe1"
+  sha256 "e52268c3e1addcadc758a5d0b96892796b0959eee3a4236238e2efcc8cf3c8e9"
   head "https://github.com/facebook/buck.git"
 
   bottle do
     root_url "https://github.com/facebook/buck/releases/download/v#{BUCK_VERSION}"
     cellar :any_skip_relocation
-    sha256 "8663afcd14676b6cb45995a2fe892664dbc90a7aff2ac9eaa51db983795f2015" => :yosemite
+    sha256 "99b5c17d3266c9b5d883154042be96f27238c092d0fe065727ecad67f5f9dabb" => :yosemite
   end
 
   depends_on "ant"
