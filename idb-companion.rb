0f084906466a2142c4bc804f57b3b5f9caedbd99
Callum Ryan <c.ryan747@gmail.com>
2019-12-04 11:37:08 +0000

    Updating idb

--- idb-companion.rb
+++ idb-companion.rb
@@ -1,17 +1,17 @@
 class IdbCompanion < Formula
   desc "A Powerful Command Line for automating iOS Simulators"
   homepage "https://github.com/facebook/idb/README.md"
-  url "https://github.com/facebook/idb/archive/v1.0.9.tar.gz"
-  sha256 "dc2ad4f9f5365c35de1a1529179830aea218e9bd4c59d0deff7d32b838ba9d76"
+  url "https://github.com/facebook/idb/archive/v1.0.10.tar.gz"
+  sha256 "0e09182a9b3cf8efc73a937edd2406ab13a86782741b0bb59ce330703462c20f"
   head "https://github.com/facebook/idb.git"
 
   depends_on :xcode => ["8.2", :build]
-  depends_on "grpc" => "1.23.0_1"
+  depends_on "grpc" => "1.25.0_1"
 
   bottle do
-    root_url "https://github.com/facebook/idb/releases/download/v1.0.9"
+    root_url "https://github.com/facebook/idb/releases/download/v1.0.10"
     cellar :any
-    sha256 "cd6527b086bd6033a5dda5e5a0edc90dd8eaf92eb71ececf0de7d1591e940ca6" => :mojave
+    sha256 "6e95179ca43e5cba3552e784b8f97e26cbe5d888b0ac56f6d0fb40d59c5e96b8" => :mojave
   end
 
   def install
