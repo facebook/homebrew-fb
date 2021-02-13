582617a677a75c214c938bd24d8ef59c5a760afc
Ruslan Sayfutdinov <sayfutdinov@fb.com>
2021-02-13 00:55:21 +0000

    Add license headers (#55)

--- buck.rb
+++ buck.rb
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 # frozen_string_literal: true
 
 class Buck < Formula
--- fbsimctl.rb
+++ fbsimctl.rb
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 class Fbsimctl < Formula
   desc "A Powerful Command Line for Managing iOS Simulators"
   homepage "https://github.com/facebook/FBSimulatorControl/fbsimctl/README.md"
