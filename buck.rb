7a2b6835ab2ccf0dbfdefa26953ce874b8efc776
Hsuan-Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2021-02-18 21:36:54 +0800

    Fix asset URL (#51)
    The previous formula needs a revision tag because it's patched, which the current latest release is not.
    
    And it affects the URL used to download assets like bottles.
    
    Fixes the missing checksum

--- buck.rb
+++ buck.rb
@@ -12,8 +12,9 @@ class Buck < Formula
   desc "Facebook's Buck build system"
   homepage "https://buckbuild.com/"
   url "https://github.com/facebook/buck/archive/v#{BUCK_VERSION}.tar.gz"
+  sha256 "c89e86e8a8355f6bc921afe8218a3cb1138c896a97e3168cf5dd220b07d8d1b5"
   license "Apache-2.0"
-  revision 1
+  revision 0
   head "https://github.com/facebook/buck.git"
 
   bottle do
