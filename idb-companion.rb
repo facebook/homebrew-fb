2483a768fc2b06faeb6cf38a06063905dec0dc29
Callum Ryan <c.ryan747@gmail.com>
2021-06-02 13:51:57 +0100

    Updating idb

--- idb-companion.rb
+++ idb-companion.rb
@@ -7,19 +7,21 @@
 class IdbCompanion < Formula
   desc "A Powerful Command Line for automating iOS Simulators"
   homepage "https://github.com/facebook/idb/README.md"
-  url "https://github.com/facebook/idb/archive/v1.1.2.tar.gz"
-  sha256 "fe52b96f22be6aa6161c1a0e09c04f34f9b5207c49cc0eb16b3e0b3c03744e42"
+  url "https://github.com/facebook/idb/archive/v1.1.3.tar.gz"
+  sha256 "4cefb5484baf108024c7287720923d796ccd490567a2dc1a3f489d66ac00e90e"
   head "https://github.com/facebook/idb.git"
 
   depends_on :xcode => ["8.2", :build]
-  depends_on "grpc" => "1.29.1"
+  depends_on "cocoapods" => ["1.10", :build]
+  depends_on "grpc" => "1.38.0"
 
   bottle do
-    root_url "https://github.com/facebook/idb/releases/download/v1.1.2"
-    sha256 cellar: :any, big_sur: "50d8dfdda62d984d50f6f095674725861224b70470bf2bc65fdcdd540b1533bb"
+    root_url "https://github.com/facebook/idb/releases/download/v1.1.3"
+    sha256 cellar: :any, big_sur: "a7b72a654177de1519165e079c3f057fbea69e9ed8b1a176952ce35ec75729b7"
   end
 
   def install
+    system "pod", "install"
     system "./idb_build.sh", "idb_companion", "build", prefix
   end
 
