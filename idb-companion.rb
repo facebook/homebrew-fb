f660049bdec9965934973052e6331119a4985096
Johnny Souza <johnnysouza@fb.com>
2022-03-03 12:00:21 +0000

    idb v1.1.7

--- idb-companion.rb
+++ idb-companion.rb
@@ -7,16 +7,18 @@
 class IdbCompanion < Formula
   desc "A Powerful Command Line for automating iOS Simulators"
   homepage "https://github.com/facebook/idb/README.md"
-  url "https://github.com/facebook/idb/archive/v1.1.6.tar.gz"
-  sha256 "000cbfe92cfafac89950e32a513d4e165cf8563945900fc34660b82f69912858"
+  url "https://github.com/facebook/idb/archive/v1.1.7.tar.gz"
+  sha256 "4eee66cb91ba0e8d9e075ba0354eedda85f20694b41f1bf4264fb491a28d511d"
   head "https://github.com/facebook/idb.git", branch: "main"
 
   depends_on :xcode => ["8.2", :build]
   depends_on "cocoapods" => ["1.10", :build]
+  depends_on "grpc" => "1.44.0"
 
   bottle do
-    root_url "https://github.com/facebook/idb/releases/download/v1.1.6"
-    sha256 cellar: :any, monterey: "4b47f89692307eb46f941fa3f0a325efe640807441053cd62e2d8b62fe528f96"
+    root_url "https://github.com/facebook/idb/releases/download/v1.1.7"
+    sha256 cellar: :any, arm64_monterey: "386fc014e107b4530b584d5e1f4ff0edec5cf408b973949c97f51f1966f1671c"
+    sha256 cellar: :any, monterey: "100c27abd8e959b5561bb8ae0425d61088ed9c07bd44c95ad5b70b4f601b79e3"
   end
 
   def install
@@ -36,4 +38,3 @@ class IdbCompanion < Formula
   end
 
 end
-
